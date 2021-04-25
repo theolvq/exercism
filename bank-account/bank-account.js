1d22a8411668e428e087473dd8f80f823eae5a53
Theo-Leveque <70675996+Theo-Leveque@users.noreply.github.com>
2021-04-25 14:31:46 -0700

    renamed bank account class properties for better clarity

--- bank-account/bank-account.js
+++ bank-account/bank-account.js
@@ -1,37 +1,38 @@
 export class BankAccount {
   constructor() {
-    this.active = false;
-    this.bal = 0;
+    this._open = false;
+    this._balance = 0;
   }
 
   open() {
-    if (this.active) throw new ValueError();
-    this.bal = 0;
-    this.active = true;
+    if (this._open) throw new ValueError();
+    this._balance = 0;
+    this._open = true;
     return this;
   }
 
   close() {
-    if (!this.active) throw new ValueError();
-    this.active = false;
+    if (!this._open) throw new ValueError();
+    this._open = false;
     return this;
   }
 
   deposit(amount) {
-    if (!this.active || amount < 0) throw new ValueError();
-    this.bal += amount;
+    if (!this._open || amount < 0) throw new ValueError();
+    this._balance += amount;
     return this;
   }
 
   withdraw(amount) {
-    if (this.bal < amount || amount < 0 || !this.active) throw new ValueError();
-    this.bal -= amount;
+    if (this._balance < amount || amount < 0 || !this._open)
+      throw new ValueError();
+    this._balance -= amount;
     return this;
   }
 
   get balance() {
-    if (!this.active) throw new ValueError();
-    return this.bal;
+    if (!this._open) throw new ValueError();
+    return this._balance;
   }
 }
 
