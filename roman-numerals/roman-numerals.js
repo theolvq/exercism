c61230641d8cc7d5e89cf7a7ebec498e6882a166
Theo-Leveque <70675996+Theo-Leveque@users.noreply.github.com>
2021-04-27 21:55:21 -0700

    completed roman numerals

---
+++ roman-numerals/roman-numerals.js
@@ -0,0 +1,26 @@
+const SPQR = {
+  M: 1000,
+  CM: 900,
+  D: 500,
+  CD: 400,
+  C: 100,
+  XC: 90,
+  L: 50,
+  XL: 40,
+  X: 10,
+  IX: 9,
+  V: 5,
+  IV: 4,
+  I: 1,
+};
+
+export const toRoman = n => {
+  let roman = '';
+  for (const [key, value] of Object.entries(SPQR)) {
+    while (value <= n) {
+      roman += key;
+      n -= value;
+    }
+  }
+  return roman;
+};
